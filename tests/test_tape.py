b23eddafcf82e7a7c1adac7f192031cfed298248
Simon Funke <simon.funke@gmail.com>
2017-03-21 13:15:20 +0100

    Revert accidently commited changes to test_tape.py

--- tests/test_tape.py
+++ tests/test_tape.py
@@ -1,21 +1,22 @@
 from fenics import *
 from fenics_adjoint import *
 
-mesh = IntervalMesh(10, 0, 1)
-V = FunctionSpace(mesh, "Lagrange", 1)
+def test_tape_visualisation():
+    mesh = IntervalMesh(10, 0, 1)
+    V = FunctionSpace(mesh, "Lagrange", 1)
 
-f = Function(V)
-f.vector()[:] = 1
+    f = Function(V)
+    f.vector()[:] = 1
 
-u = TrialFunction(V)
-u_ = Function(V)
-v = TestFunction(V)
-bc = DirichletBC(V, Constant(1), "on_boundary")
+    u = TrialFunction(V)
+    u_ = Function(V)
+    v = TestFunction(V)
+    bc = DirichletBC(V, Constant(1), "on_boundary")
 
-a = f*inner(grad(u), grad(v))*dx
-L = f*v*dx
-for i in range(3):
+    a = f*inner(grad(u), grad(v))*dx
+    L = f*v*dx
     solve(a == L, u_, bc)
+    project(u_, V)
 
-tape = get_working_tape()
-tape.visualise() 
+    tape = get_working_tape()
+    tape.visualise(filename="graph.pdf")
