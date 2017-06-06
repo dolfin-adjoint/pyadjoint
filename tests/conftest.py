2f0aff833e4b60e7fbb5566056640a83975c3c9c
Simon Funke <simon.funke@gmail.com>
2017-06-06 15:43:31 +0200

    Fix spacing issues

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import importlib
 def skip_by_missing_module(request):
     if request.node.get_marker('skipif_module_is_missing'):
         to_import = request.node.get_marker('skipif_module_is_missing').args[0]
-	try:
-	    importlib.import_module(to_import)
-	except ImportError:
+    try:
+        importlib.import_module(to_import)
+    except ImportError:
             pytest.skip('skipped because module {} is missing'.format(to_import))   
