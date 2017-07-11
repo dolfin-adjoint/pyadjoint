fe88f6706b3973e2737449aca2b2a7a18c31de27
Knut <knut.helland@gmail.com>
2017-07-11 14:11:24 +0200

    simplified import structure

--- pyadjoint/__init__.py
+++ pyadjoint/__init__.py
@@ -6,4 +6,8 @@ __maintainer__ = 'Sebastian Kenji Mitusch'
 __email__ = 'sebastkm@math.uio.no'
 
 from .block import Block
-from .tape import Tape
+from .tape import Tape, set_working_tape, get_working_tape
+from .adjfloat import AdjFloat
+from .reduced_functional import ReducedFunctional
+from .drivers import compute_gradient, Hessian
+from .verification import taylor_test, taylor_test_multiple
