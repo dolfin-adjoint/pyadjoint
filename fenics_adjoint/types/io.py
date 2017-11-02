c979bce1115da535262c7cca79cd21ce0653b434
Sebastian <s.mitusch@hotmail.com>
2017-11-02 18:12:29 +0100

    io: create new block_outputs for HDF5File.read coefficients

---
+++ fenics_adjoint/types/io.py
@@ -0,0 +1,17 @@
+import backend
+from pyadjoint.tape import annotate_tape
+
+
+__HDF5File_read__ = backend.HDF5File.read
+
+
+def HDF5File_read(self, *args, **kwargs):
+    annotate = annotate_tape(kwargs)
+    output = __HDF5File_read__(self, *args, **kwargs)
+
+    if annotate:
+        func = args[0]
+        func.create_block_output()
+    return output
+
+backend.HDF5File.read = HDF5File_read
