1eb85715c2bec83fe07a94fa3ff94650eb064cf9
jorgensd <dokken92@gmail.com>
2018-03-06 17:56:47 +0100

    Added some penalization. There are problems with gradient of double-clamped, guess because it is very small

--- examples/elasticity-shape/linear_elasticity.py
+++ examples/elasticity-shape/linear_elasticity.py
@@ -5,12 +5,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 """Prepares 2D geometry. Returns facet function with 1, 2 on parts of
 the boundary."""
-n = 100
+n = 50
 length = 2
 x0 = 0.0
 x1 = x0 + length
+height = 0.2
 y0 = 0.0
-y1 = 0.2
+y1 = y0 + height
+
 mesh = RectangleMesh(Point(x0, y0), Point(x1, y1), int((x1-x0)*n), int((y1-y0)*n), 'crossed')
 mesh = Mesh(mesh)
 boundary_parts = MeshFunction('size_t', mesh, mesh.geometry().dim()-1)
@@ -54,7 +56,8 @@ bcLeft = DirichletBC(V, c, boundary_parts, 1)
 bcRight = DirichletBC(V, c, boundary_parts, 2)
 # Create solution function
 u_fin = Function(V, name="deform")
-solve(a==L, u_fin, bcs=[bcLeft, bcRight])
+solve(a==L, u_fin, bcs=[bcLeft])
+#solve(a==L, u_fin, bcs=[bcLeft, bcRight])
 
 File("elasticity.pvd", "compressed") << u_fin
 
@@ -63,47 +66,57 @@ File("elasticity.pvd", "compressed") << u_fin
 # import dolfin
 # stress = dolfin.project(sigma(u), V=W)
 # File("stress.pvd") << stress
-
+alpha_reg = 1e2
+alpha_vol = 1e1
+one = interpolate(Constant(1.0), FunctionSpace(mesh, "CG", 1))
 J = assemble(inner(u_fin,u_fin)*dx)
+J += alpha_reg*assemble(inner(s,s)*dx)
+J += alpha_vol*(assemble(one*dx)-height*length)*(assemble(one*dx)-height*length)
 Jhat = ReducedFunctional(J, Control(s))
 Jhat.optimize()
-def riesz_representation(value):
-    u,v = TrialFunction(V), TestFunction(V)
-    alpha = 1
-    a = Constant(alpha)*inner(grad(u), grad(v))*dx+inner(u,v)*dx
-    A = assemble(a)
-    [bc.apply(A,value) for bc in [bcLeft, bcRight]]
-    representation = Function(V)
-    solve(A, representation.vector(), value)
-    return representation
 
 n = VolumeNormal(mesh)
-bcs = DirichletBC(V, Constant((0.0,0.0)), boundary_parts, 1)
 s2 = Function(V)
-s2.interpolate(Expression(("2*(x[0]-x0)*(x1-x[0])","5*(x[0]-x0)*(x1-x[0])"), x0=x0,x1=x1, degree=2))
-[bc.apply(s2.vector()) for bc in [bcLeft, bcRight]]
-plot(s2)
-plt.show()
+# s2.interpolate(Expression(("0","0.02*x[0]*x[0]*(length-x[0])*(length-x[0])"), length=length, degree=2))
+#[bc.apply(s2.vector()) for bc in [bcLeft, bcRight]]
+s2.interpolate(Expression(("5*x[0]","5*x[0]*x[1]"), x0=x0, degree=2))
+[bc.apply(s2.vector()) for bc in [bcLeft]]
 s0 = Function(V)
 taylor_test(Jhat, s0, s2, dJdm=0)
 print("-"*10)
 taylor_test(Jhat, s0, s2)
 
+def riesz_representation(value):
+    u,v = TrialFunction(V), TestFunction(V)
+    alpha = 1
+    a = Constant(alpha)*inner(grad(u), grad(v))*dx+inner(u,v)*dx
+    A = assemble(a)
+    # [bc.apply(A,value) for bc in [bcLeft, bcRight]]
+    [bc.apply(A,value) for bc in [bcLeft]]
+
+    representation = Function(V)
+    solve(A, representation.vector(), value)
+    return representation
 
 
-it,max_it = 0, 100
+it,max_it = 0, 50
 min_stp = 1e-6
 meshout = File("output/mesh.pvd")
 meshout << mesh
-Js = [Jhat(s)]
-print(Js[0])
+Js = []
+
 import time
 move = Function(V)
 while it <= max_it:
     print("-"*5, "Iteration %d" %it, "-"*5)
+    if it == 0:
+        J_step = Jhat(s)
+        print(J_step)
+    Js.append(J_step)
+    meshout << mesh
     dJdm = Jhat.derivative(options={"riesz_representation":
                                     riesz_representation})
-    step =  1
+    step =  1e-1
     while (step>min_stp):
         dJdm_step = Function(V)
         dJdm_step.vector()[:] = -step*dJdm.vector()
@@ -120,8 +133,10 @@ while it <= max_it:
             # Reset mesh and half step-size
             move.vector()[:] -= dJdm_step.vector()
             step /=2
-    Js.append(J_step)
-    meshout << mesh
+            if step<=min_stp:
+                print("Minimum step-length reached")
+                it = max_it+1
+
     it+=1
 
 
