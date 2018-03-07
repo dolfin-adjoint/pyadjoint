f22bcdbaaff47f039572ecdadab4fb75c700ce00
Johannes Ring <johannr@simula.no>
2018-03-07 13:16:02 +0100

    Remove old code for visualising the tape

--- tests/fenics_adjoint/test_tape.py
+++ tests/fenics_adjoint/test_tape.py
@@ -22,28 +22,7 @@ def test_tape_visualisation():
     project(u_, V)
 
     tape = get_working_tape()
-    tape.visualise(filename="graph.pdf")
-
-@pytest.mark.skipif_module_is_missing("pygraphviz")
-def test_tape_visualisation():
-    mesh = IntervalMesh(10, 0, 1)
-    V = FunctionSpace(mesh, "Lagrange", 1)
-
-    f = Function(V)
-    f.vector()[:] = 1
-
-    u = TrialFunction(V)
-    u_ = Function(V)
-    v = TestFunction(V)
-    bc = DirichletBC(V, Constant(1), "on_boundary")
-
-    a = f*inner(grad(u), grad(v))*dx
-    L = f*v*dx
-    solve(a == L, u_, bc)
-    project(u_, V)
-
-    tape = get_working_tape()
-    tape.visualise(filename="graph.dot", dot=True)
+    tape.visualise()
 
 def test_tape_time():
     set_working_tape(Tape())
