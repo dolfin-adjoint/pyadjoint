1ac9b4f172187d8f0a76804245305c4134128401
jorgensd <dokken92@gmail.com>
2018-03-09 17:29:22 +0100

    Fixed pyadjoint version for Neumann-conditions, now deformation-schemes work better than boundary movement, after fixing the ruined mesh

--- examples/heat-shape/Demo_pyadjoint.py
+++ examples/heat-shape/Demo_pyadjoint.py
@@ -10,10 +10,13 @@ import numpy as np
 c = [0.5,0.5]
 rot_c = [c[0]-0.1, c[1]]
 rot_center = Point(rot_c[0],rot_c[1])
-VariableBoundary = 2
-FixedBoundary = 1
-N, r = 200, 0.05
+VariableBoundary = 1
+FixedBoundary = 2
+N, r = 100, 0.1
 L, H = 1,1
+
+# neumann = False
+neumann = True
 with open("mesh.geo", 'r') as file:
     data = file.readlines()
     data[0] = "lc1 = %s;\n" %(float(1/N))
@@ -24,8 +27,8 @@ with open("mesh.geo", 'r') as file:
 
     data[7] = "L = %s;\n" %(float(L))
     data[8] = "H = %s;\n" %(float(H))
-    data[55] = "bc = %s;\n" %int(FixedBoundary)
-    data[56] = "object = %s;\n" %int(VariableBoundary)
+    data[54] = "bc = %s;\n" %int(FixedBoundary)
+    data[55] = "object = %s;\n" %int(VariableBoundary)
     with open("mesh.geo", 'w') as file:
         file.writelines( data )
 os.system("gmsh -2 mesh.geo -o mesh.msh")
@@ -33,7 +36,7 @@ os.system("dolfin-convert mesh.msh mesh.xml")
 
 mesh =  Mesh("mesh.xml")
 
-f = Expression("100*x[0]*sin(x[0])*cos(x[1])", degree=4)
+f = Expression("100*x[0]*sin(x[0])*cos(x[1])", degree=4, name="f")
 
 S = VectorFunctionSpace(mesh, "CG", 1)
 s = Function(S)
@@ -42,14 +45,16 @@ ALE.move(mesh, s)
 # Setup
 V = FunctionSpace(mesh, "CG", 1)
 u, v = TrialFunction(V), TestFunction(V)
-F = inner(grad(u), grad(v))*dx  - f*v*dx + u*v*dx
+F = inner(grad(u), grad(v))*dx  - f*v*dx # + u*v*dx
 
 # FIXME: Handling of Dirichlet Conditions
 marker = MeshFunction("size_t", mesh, "mesh_facet_region.xml")
-bcs = [DirichletBC(V, Constant(0), marker, FixedBoundary)]#,
-       # DirichletBC(V, Constant(1), marker, VariableBoundary)]
-
-
+if neumann:
+    bcs = [DirichletBC(V, Constant(0), marker, FixedBoundary)]
+else:
+    bcs = [DirichletBC(V, Constant(0), marker, FixedBoundary),
+           DirichletBC(V, Constant(1), marker, VariableBoundary)]
+    
 T = Function(V, name="T")
 
 solve(lhs(F) == rhs(F), T, bcs=bcs)
@@ -61,12 +66,14 @@ Jhat = ReducedFunctional(J, Control(s))
 n = VolumeNormal(mesh)
 s2 = Function(S)
 print(Jhat(s))
-s2.vector()[:] = -0.5*n.vector()[:]
 
+s2.vector()[:] = -0.5*n.vector()[:]
+bcs = DirichletBC(VectorFunctionSpace(mesh, "CG", 1), Constant((0,0)), marker, FixedBoundary)
+bcs.apply(s2.vector())
 
 #-------------- Super important hand-written taylortest----------------
 dJdm = Jhat.derivative()
-step = 0.001
+step = 0.8/N
 boundary_move = s2.copy(deepcopy=True)
 boundary_move.vector()[:] *= step
 
@@ -80,7 +87,6 @@ J_boundary = Jhat(boundary_move)
 print(dJdm.vector().inner(boundary_move.vector()), J_boundary-Jhat(s), J_boundary)
 J_s = Jhat(s3)
 print(dJdm.vector().inner(s3.vector()), J_s-Jhat(s), J_s)
-
 plot(mesh)
 Jhat(boundary_move)
 plot(mesh, color="g")
