909c12036e1866bc7dbfd253b0f5c1faf8712a44
jorgensd <dokken92@gmail.com>
2018-03-16 11:31:47 +0100

    Cleanup in elasticity problem, Taylor not giving correct convergence, but optimization gives good result

--- examples/3Delasiticty-shape/linear_elasticity.py
+++ examples/3Delasiticty-shape/linear_elasticity.py
@@ -2,7 +2,7 @@ from dolfin import *
 import matplotlib.pyplot as plt
 from dolfin_adjoint import *
 from femorph import *
-n = 3
+n = 5
 length = 1 #m
 width = 0.2 #m 
 height = 0.05 #m
@@ -29,22 +29,21 @@ def geometry_3d():
     left  = AutoSubDomain(lambda x: near(x[0], X0))
     right = AutoSubDomain(lambda x: near(x[0], X1))
     top = AutoSubDomain(lambda x: near(x[2], Z1))
-    # sides = AutoSubDomain(lambda x: x[0]<0.1*x1 and near(x[1], Z1)) 
     top.mark(boundary_parts, 3)
     left.mark(boundary_parts, 1)
     right.mark(boundary_parts, 2)
-    # sides.mark(boundary_parts,4)
     return mesh, boundary_parts
 
 
 mesh, boundary_parts = geometry_3d()
 
-# Rotation rate and mass density
+
+# Loading due to gravity
 rho = 2700 #kg/m^3 aluminium
 g = 9.81
-# Loading due to gravity
 f = Expression(("0", "0", "-rho*g"),
                g=g, rho=rho, degree=2)
+
 # Elasticity parameters
 E = 6.894757e10 # Youngs modulus aluminium
 nu = 0.33 # Poisson ratio aluminium
@@ -61,64 +60,43 @@ s = Function(V)
 ALE.move(mesh, s)
 
 stress = Expression(("0", "0", "-4e7*exp(-pow((x[0]-length/5),2)/0.01)*exp(-pow((x[1]-width/2),2)/0.001)"), width=width, length=length, degree=2, domain=mesh) # N/m^2s
-clamp = Expression(("0","0","-1"),degree=2,domain=mesh)
 dS_stress = Measure("ds", domain=mesh, subdomain_data=boundary_parts)
 
 # Define variational problem
 u = TrialFunction(V)
 v = TestFunction(V)
 a = inner(sigma(u), grad(v))*dx
-L = inner(f, v)*dx  + inner(stress, v)*dS_stress(3)# + inner(clamp,v)*dS_stress(4)
+L = inner(f, v)*dx  + inner(stress, v)*dS_stress(3)
 
 # Set up boundary condition on inner surface
 c = Constant((0.0, 0.0, 0.0))
 bcLeft = DirichletBC(V, c, boundary_parts, 1)
 bcRight = DirichletBC(V, c, boundary_parts, 2)
+
 # Create solution function
 u_fin = Function(V, name="deform")
 solve(a==L, u_fin, bcs=[bcLeft, bcRight], solver_parameters={"linear_solver": "mumps"})
-# solve(a==L, u_fin, bcs=[bcLeft], solver_parameters={"linear_solver": "mumps"})
 
 elas = File("elasticity_new_functional.pvd", "compressed")
 elas << u_fin
-# ALE.move(mesh, u_fin)
-# J = assemble(Expression("x[0]", degree=1)*dx(domain=mesh))
-# J = assemble(inner(u_fin,u_fin)*dx)+\
-#     2.5*assemble(inner(s,s)*ds)
-J = assemble(inner(sigma(u_fin),sym(grad(u_fin)))*dx) + 1e3*(assemble(1*dx(domain=mesh))-length*height*width)**2
-
+J = assemble(inner(sigma(u_fin),sym(grad(u_fin)))*dx)
 Jhat = ReducedFunctional(J, Control(s))
 Jhat.optimize()
 # s2 = Function(V)
-# s2.interpolate(Expression(("0", "0", "75*(x[2]-height/2)*sin(2*pi*x[0])"), degree=2, height=height))
+# s2.interpolate(Expression(("0", "0", "sin(2*pi/length*x[0])"), degree=2, length=length))
 # [bc.apply(s2.vector()) for bc in [bcLeft, bcRight]]
 
-# s2.vector()[:] *= 0.01
 # s0 = Function(V)
+# s2.vector()[:]*=0.01
+# ALE.move(mesh,s2)
+# plot(mesh)
+# plt.show()
 # taylor_test(Jhat, s0, s2, dJdm=0)
 # print("-"*10)
 # taylor_test(Jhat, s0, s2)
 # exit(1)
-
 
 tape.visualise("tape_linear.dot", dot=True)
-n = VolumeNormal(mesh)
-
-# s2.interpolate(Expression(("0", "0", "75*(x[2]-height/2)*sin(2*pi*x[0])"), degree=2, height=height))
-# [bc.apply(s2.vector()) for bc in [bcLeft, bcRight]]
-
-# mfile = File("mesh.pvd")
-# mfile << mesh
-# s2.vector()[:] *= 0.01
-# ALE.move(mesh, s2)
-# mfile << mesh
-# s2.vector()[:] *= 100
-s0 = Function(V)
-# Jhat(s0)
-# taylor_test(Jhat, s0, s2, dJdm=0)
-# print("-"*10)
-# taylor_test(Jhat, s0, s2)
-# exit(1)
 
 def riesz_representation(value):
     u,v = TrialFunction(V), TestFunction(V)
@@ -126,7 +104,6 @@ def riesz_representation(value):
     a = Constant(alpha)*inner(grad(u), grad(v))*dx+inner(u,v)*dx
     A = assemble(a)
     [bc.apply(A,value) for bc in [bcLeft, bcRight]]
-    # [bc.apply(A,value) for bc in [bcLeft]]
     representation = Function(V)
     solve(A, representation.vector(), value)
     return representation
@@ -140,7 +117,11 @@ move = Function(V)
 move_step = Function(V)
 Js = [Jhat(move)]
 meshout << mesh
-
+stress_out = Function(V)
+bc_stress = DirichletBC(V, stress, boundary_parts, 3)
+bc_stress.apply(stress_out.vector())
+stress_file = File("stress.pvd")
+stress_file << stress_out
 while it <= max_it and red > red_tol:
     print("-"*5, "Iteration %d" %it, "-"*5)
 
@@ -180,8 +161,6 @@ print("-"*5, "Optimization Finished", "-"*5)
 print(Js[0], Js[-1])
 print(Jhat(s0),Jhat(move))
 
-# solve(a==L, u_fin, bcs=[bcLeft])
 solve(a==L, u_fin, bcs=[bcLeft, bcRight])
-File("stress.pvd") << interpolate(stress, S)
+stress_file << stress_out
 elas << u_fin
-
