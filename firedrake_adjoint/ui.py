3ea79c1e8b201b4066f486ed04ab3372a9dd1263
Patrick Farrell <patrick@pefarrell.org>
2018-07-20 20:50:59 +0100

    Import some optimisation stuff into firedrake_adjoint.

--- firedrake_adjoint/ui.py
+++ firedrake_adjoint/ui.py
@@ -13,9 +13,10 @@ from pyadjoint.tape import (Tape, set_working_tape, get_working_tape,
                             pause_annotation, continue_annotation)
 from pyadjoint.reduced_functional import ReducedFunctional
 from pyadjoint.verification import taylor_test
-from pyadjoint.drivers import compute_gradient, compute_hessian 
+from pyadjoint.drivers import compute_gradient, compute_hessian
 from pyadjoint.adjfloat import AdjFloat
 from pyadjoint.control import Control
+from pyadjoint import IPOPTSolver, ROLSolver, MinimizationProblem, InequalityConstraint
 
 tape = Tape()
 set_working_tape(tape)
