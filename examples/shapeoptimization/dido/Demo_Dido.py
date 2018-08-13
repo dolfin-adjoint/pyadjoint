29f03b3691c57938f9e1518b76d506fbc1e93f65
dokken <dokken92@gmail.com>
2018-08-13 09:29:45 +0200

    Moved dido demo to shape optimization folder

--- examples/shapeoptimization/dido/Demo_Dido.py
+++ examples/shapeoptimization/dido/Demo_Dido.py
@@ -1,30 +1,27 @@
 from dolfin import *
 from dolfin_adjoint import *
-from femorph import *
-import matplotlib.pyplot as plt
 
-N = 100
-mesh = Mesh(UnitSquareMesh(N, N))
-One = Constant(1.0)
+N = 50
+mesh = UnitSquareMesh(N, N)
 
 V = VectorFunctionSpace(mesh, "CG", 1)
 s0 = Function(V)
 ALE.move(mesh, s0)
-print(assemble(One*dx(domain=mesh)))
 
-alpha = 1e1
-J = -assemble(One*dx(domain=mesh)) + alpha*(assemble(One*ds(domain=mesh)) - 4)**2
+
+One = Constant(1.0)
+J = -assemble(One*dx(domain=mesh))
+
+# Weak Penalization of volume offset
+alpha = 1
+J += alpha*(assemble(One*ds(domain=mesh)) - 4)**2
 Jhat = ReducedFunctional(J, Control(s0))
 
-s = interpolate(Expression(("10*x[0]", "10*x[1]"), degree=2), V)
-taylor_test(Jhat, s0, s, dJdm=0)
-taylor_test(Jhat, s0, s)
-
-
 it, max_it = 1, 100
-min_stp = 1e-6
+min_stp = 1e-6 
 red_tol = 1e-6
 red = 2*red_tol
+
 move = Function(V)
 move_step = Function(V)
 Js = [Jhat(move)]
@@ -32,6 +29,9 @@ meshout = File("output/mesh.pvd")
 meshout << mesh
 
 def riesz_representation(value):
+    """
+    Smoothed H1 Riesz Representation of a shape gradient
+    """
     V = VectorFunctionSpace(mesh, "CG", 1)
     u,v = TrialFunction(V), TestFunction(V)
     alpha = 1
@@ -43,14 +43,12 @@ def riesz_representation(value):
 
 
 while it <= max_it and red > red_tol:
-    print("-"*5, "Iteration %d" %it, "-"*5)
-
     # Compute derivative of previous configuration
     dJdm = Jhat.derivative(options={"riesz_representation":
                                     riesz_representation})
 
     # Linesearch
-    step = 5
+    step = 1
     while step > min_stp:
         # Evaluate functional at new step 
         move_step.vector()[:] = move.vector() - step*dJdm.vector()
@@ -71,7 +69,13 @@ while it <= max_it and red > red_tol:
     red = abs((Js[-1] - Js[-2])/Js[-1])
     it += 1
 
-print("Relative reduction: %.2e" %red)
+
+print("Total number of iterations: %d" % (it-1))
 print("-"*5, "Optimization Finished", "-"*5)
-print(Js[0], Js[-1])
-print(Jhat(s),Jhat(move))
+print("Initial Functional value: %.2f" % Js[0])
+print("Final Functional value: %.2f" % Js[-1])
+
+# Taylor-test of shape derivative
+s = interpolate(Expression(("sin(x[0])", "10*cos(x[1])"), degree=2), V)
+taylor_test(Jhat, s0, s, dJdm=0)
+taylor_test(Jhat, s0, s)
