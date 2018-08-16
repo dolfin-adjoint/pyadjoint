a082c5f752246ab9578f98d6f9eb6849845182ba
dokken <dokken92@gmail.com>
2018-08-16 18:17:26 +0200

    Updated demo for failing solving

--- tests/fenics_adjoint/shape_hessian.py
+++ tests/fenics_adjoint/shape_hessian.py
@@ -26,7 +26,7 @@ f = Function(S, name="W")
 f.interpolate(Expression(("A*sin(x[1])", "A*cos(x[1])"),degree=2,A=10))
 h = Function(S,name="V")
 h.interpolate(Expression(("A*cos(x[1])", "A*x[1]"),degree=2,A=10))
-
+tape.visualise("hessian_solve.dot", dot=True)
 
 # Finite difference
 r0 = taylor_test(Jhat, s, h, dJdm=0)
@@ -34,9 +34,9 @@ Jhat(s)
 assert(r0>0.95)
 # First order taylor
 s.tlm_value = h
-tape.evaluate_tlm()
-r1 = taylor_test(Jhat, s, h, dJdm=J.block_variable.tlm_value)
-assert(r1>1.95)
+#tape.evaluate_tlm()
+#r1 = taylor_test(Jhat, s, h, dJdm=J.block_variable.tlm_value)
+# assert(r1>1.95)
 Jhat(s)
 
 # Second order taylor
@@ -47,7 +47,7 @@ Hm = compute_hessian(J, c, h).vector().inner(h.vector())
 dJdmm_exact = derivative(derivative(sin(X[1])* dx(domain=mesh),X,h), X, h)
 
 
-# ---------------- hessian for assembly ----------------------
+# # ---------------- hessian for assembly ----------------------
 # import numpy as np
 # from dolfin import *
 # from dolfin_adjoint import *
@@ -65,9 +65,9 @@ dJdmm_exact = derivative(derivative(sin(X[1])* dx(domain=mesh),X,h), X, h)
 # Jhat = ReducedFunctional(J, c)
 
 # f = Function(S, name="W")
-# f.interpolate(Expression(("A*sin(x[1])", "A*cos(x[1])"),degree=2,A=A))
+# f.interpolate(Expression(("A*sin(x[1])", "A*cos(x[1])"),degree=2,A=10))
 # h = Function(S,name="V")
-# h.interpolate(Expression(("A*cos(x[1])", "A*x[1]"),degree=2,A=A))
+# h.interpolate(Expression(("A*cos(x[1])", "A*x[1]"),degree=2,A=10))
 
 
 # # Finite difference
