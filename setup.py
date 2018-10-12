e42c79d0727ca24a73de6f38221abcafdb47a39e
Min RK <benjaminrk@gmail.com>
2018-10-12 15:10:54 +0200

    tensorflow is optional as well
    add `pyadjoint[all]` for pulling in all optional dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+from itertools import chain
 from setuptools import setup
+
+extras = {
+    'moola': ['moola>=0.1.6'],
+    'test': ['pytest'],
+    'doc': ['sphinx', 'sphinxcontrib-bibtex'],
+    'visualisation': ['tensorflow'],
+}
+# 'all' includes all of the above
+extras['all'] = list(chain(*extras.values()))
 
 setup(name='dolfin_adjoint',
       version='2018.1.0',
@@ -14,10 +24,6 @@ setup(name='dolfin_adjoint',
                 'pyadjoint.optimization'],
       package_dir={'fenics_adjoint': 'fenics_adjoint', 'pyadjoint': 'pyadjoint',
                    'firedrake_adjoint': 'firedrake_adjoint', 'dolfin_adjoint': 'dolfin_adjoint'},
-      install_requires=['scipy', 'tensorflow'],
-      extras_require={
-        'moola': ['moola>=0.1.6'],
-        'test': ['pytest'],
-        'doc': ['sphinx', 'sphinxcontrib-bibtex'],
-      },
-      )
+      install_requires=['scipy'],
+      extras_require=extras,
+)
