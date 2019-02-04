510bfb28e1a2a0e2d58bb29643b42a8599cc2e8f
jorgensd <dokken92@gmail.com>
2019-02-04 15:26:42 +0100

    Fixed strange flake8 local issue

--- pyadjoint/verification.py
+++ pyadjoint/verification.py
@@ -63,8 +63,8 @@ def convergence_rates(E_values, eps_values, show=True):
     from numpy import log
     r = []
     for i in range(1, len(eps_values)):
-        r.append(log(E_values[i] / E_values[i - 1]) /
-                 log(eps_values[i] / eps_values[i - 1]))
+        r.append(log(E_values[i] / E_values[i - 1])
+                 / log(eps_values[i] / eps_values[i - 1]))
     if show:
         print("Computed convergence rates: {}".format(r))
     return r
