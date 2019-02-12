cbaed0e9039d097e85a8b821550dd0f4a6750cd9
sebastkm <s.mitusch@hotmail.com>
2019-02-12 14:24:11 +0100

    projection: fix bug for multiple domain projections

--- fenics_adjoint/projection.py
+++ fenics_adjoint/projection.py
@@ -33,9 +33,13 @@ def project(*args, **kwargs):
 
 class ProjectBlock(SolveBlock):
     def __init__(self, v, V, output, bcs=[], *args, **kwargs):
+        mesh = kwargs.pop("mesh", None)
+        if mesh is None:
+            mesh = V.mesh()
+        dx = backend.dx(mesh)
         w = backend.TestFunction(V)
         Pv = backend.TrialFunction(V)
-        a = backend.inner(w, Pv) * backend.dx
-        L = backend.inner(w, v) * backend.dx
+        a = backend.inner(w, Pv) * dx
+        L = backend.inner(w, v) * dx
 
         super(ProjectBlock, self).__init__(a == L, output, bcs, *args, **kwargs)
--- tests/fenics_adjoint/test_projection.py
+++ tests/fenics_adjoint/test_projection.py
@@ -31,3 +31,30 @@ def test_projection():
 	dJdf = f.adj_value
 	# TODO: This test does nothing. Make it actually test projection.
 	#print(dJdf.array())
+
+
+def test_multiple_meshes():
+	mesh = UnitSquareMesh(20, 20)
+	S1 = FiniteElement('CG', mesh.ufl_cell(), 1)
+	Y = FunctionSpace(mesh, S1)
+
+	t = Constant(2.0)
+	y_expr = Expression('x[1]*t', t=t, degree=3, name='y')
+	y_expr.user_defined_derivatives = {t: Expression('x[1]', degree=3, name='dy')}
+
+	y = project(y_expr, Y)
+
+	mesh_2 = UnitSquareMesh(40, 40)
+	Y_2 = FunctionSpace(mesh_2, S1)
+
+	project(y, Y_2)
+
+	J = assemble(y**4*dx)
+	Jhat = ReducedFunctional(J, Control(t))
+
+	h = Constant(1)
+	results = taylor_to_dict(Jhat, t, h)
+
+	assert min(results["FD"]["Rate"]) > 0.9
+	assert min(results["dJdm"]["Rate"]) > 1.9
+	assert min(results["Hm"]["Rate"]) > 2.9
