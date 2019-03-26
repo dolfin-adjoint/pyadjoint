1de3c82c8e4c3a5598624117e86414816202dc8d
jorgensd <dokken92@gmail.com>
2019-03-26 10:50:10 +0100

    Updated functionassigner test to compare with splitblock, as well as fixing the manual evaluate tlm test

--- tests/fenics_adjoint/test_functionassigner.py
+++ tests/fenics_adjoint/test_functionassigner.py
@@ -1,5 +1,6 @@
 from dolfin import *
 from dolfin_adjoint import *
+import numpy as np
 
 def test_function_assigner_poisson():
     mesh = UnitSquareMesh(15,15)
@@ -34,24 +35,30 @@ def test_function_assigner_poisson():
     J = assemble(uh*ds) + assemble(uh*uh**2*dx)# + assemble(inner(f,f)*dx)
 
     Jhat = ReducedFunctional(J, Control(s_))
+    dJ1 = Jhat.derivative()
 
     from pyadjoint.tape import stop_annotating
     with stop_annotating():
         A = 1
         pert = project(A*Expression(("x[0]","cos(pi*x[1])"),degree=3), S)
-
         results = taylor_to_dict(Jhat, s_, pert)
-        print(results)
         assert(min(results["FD"]["Rate"]) > 0.95)
         assert(min(results["dJdm"]["Rate"]) > 1.95)
         assert(min(results["Hm"]["Rate"]) > 2.95)
-        # s_.tlm_value = pert.copy(deepcopy=True)
-        # tape = get_working_tape()
-        # tape.evaluate_tlm()
-        # r1_tlm = taylor_test(Jhat, s_, pert, dJdm=J.block_variable.tlm_value)
-        # print(r1_tlm)
-        # assert(False)
-        # assert(min(results["Hm"]["Rate"]) > 2.95)
 
+    tape = get_working_tape()
+    tape.reset_tlm_values()
+    s_.tlm_value = pert
+    tape.evaluate_tlm()
+    r1_tlm = taylor_test(Jhat, s_, pert, dJdm=J.block_variable.tlm_value)
+    assert(r1_tlm > 1.95)
+    Jhat(s_)
+    # Solve same problem with split
+    uh, rh = ur.split()
+    J = assemble(uh*ds) + assemble(uh*uh**2*dx)
+
+    Jhat = ReducedFunctional(J, Control(s_))
+    dJ2 = Jhat.derivative()
+    assert(np.allclose(dJ1.vector().get_local(), dJ2.vector().get_local()))
 if __name__ == "__main__":
      test_function_assigner_poisson()
