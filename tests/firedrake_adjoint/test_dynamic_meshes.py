83b6b938535ab614f5b4188d2d33bc9aa9cbb9f0
jorgensd <dokken92@gmail.com>
2019-06-05 14:50:04 +0200

    FixIssue 116, overloading RectangleMesh.create, as well as adding bugfix to firedrake test

--- tests/firedrake_adjoint/test_dynamic_meshes.py
+++ tests/firedrake_adjoint/test_dynamic_meshes.py
@@ -46,9 +46,10 @@ def test_dynamic_meshes_2D(mesh):
                   project(C*as_vector((-x[0]**2, x[1])), S)]
         zero = [Function(S),Function(S), Function(S)]
         results = taylor_to_dict(Jhat, zero, taylor)
+    print(results)
     assert(np.mean(results["FD"]["Rate"])>0.9)
     assert(np.mean(results["dJdm"]["Rate"])>1.9)
-    assert(np.mean(results["Hm"]["Rate"])>0.9)
+    assert(np.mean(results["Hm"]["Rate"])>2.9)
 
 
 @pytest.mark.parametrize("mesh", [UnitCubeMesh(4,4,5),
@@ -108,5 +109,5 @@ def test_dynamic_meshes_3D(mesh):
     print(results)
     assert(np.mean(results["FD"]["Rate"])>0.9)
     assert(np.mean(results["dJdm"]["Rate"])>1.9)
-    assert(np.mean(results["Hm"]["Rate"])>0.9)
+    assert(np.mean(results["Hm"]["Rate"])>2.9)
 
