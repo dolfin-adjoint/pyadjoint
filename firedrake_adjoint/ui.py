dc03ea209a1934c62efbffea916655d7ed00d3af
David Ham <David.Ham@imperial.ac.uk>
2019-12-17 16:47:12 +0000

    remove fenics_adjoint

--- firedrake_adjoint/ui.py
+++ firedrake_adjoint/ui.py
@@ -1,16 +1,4 @@
 # flake8: noqa F401
-from fenics_adjoint.assembly import assemble, assemble_system
-from fenics_adjoint.solving import solve
-from fenics_adjoint.projection import project
-from fenics_adjoint.types import Constant, DirichletBC
-from fenics_adjoint.variational_solver import (NonlinearVariationalProblem, NonlinearVariationalSolver,
-                                               LinearVariationalProblem, LinearVariationalSolver)
-from fenics_adjoint.interpolation import interpolate
-from fenics_adjoint.ufl_constraints import UFLInequalityConstraint, UFLEqualityConstraint
-
-from firedrake_adjoint.types.expression import Expression
-from firedrake_adjoint.types.function import Function
-
 from pyadjoint.tape import (Tape, set_working_tape, get_working_tape,
                             pause_annotation, continue_annotation)
 from pyadjoint.reduced_functional import ReducedFunctional
