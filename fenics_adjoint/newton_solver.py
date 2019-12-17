5ebeef6e476b240372daea44959017bc2ef19a1c
dokken <dokken92@gmail.com>
2019-12-17 16:44:15 +0000

    Fixed NewtonSolver parameter handling

--- fenics_adjoint/newton_solver.py
+++ fenics_adjoint/newton_solver.py
@@ -21,8 +21,20 @@ class NewtonSolver(backend.NewtonSolver):
             u = vec.function
 
             sb_kwargs = SolveBlock.pop_kwargs(kwargs)
+            params = self.parameters.to_dict()
+            def unravel_dict(params):
+                non_transfer_param = ["krylov_solver", "lu_solver"]
+                for param in non_transfer_param:
+                    if param in params.keys():
+                        params.pop(param)
+                for key in params.keys():
+                    if isinstance(params[key], dict):
+                        params[key] = unravel_dict(params[key])
+                    else:
+                        params[key] = params[key].value()
+            unravel_dict(params)
             block = SolveBlock(F == 0, u, bcs,
-                               solver_parameters={"newton_solver": self.parameters.to_dict()},
+                               solver_parameters={"newton_solver": params},
                                **sb_kwargs)
             tape.add_block(block)
 
