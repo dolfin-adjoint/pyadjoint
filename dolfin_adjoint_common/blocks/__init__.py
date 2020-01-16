8abcb2d0679bb5c170c64ef290ea927d2c1bcbe5
Sebastian Mitusch <s.mitusch@hotmail.com>
2020-01-16 16:35:34 +0100

    common: remove expression block

--- dolfin_adjoint_common/blocks/__init__.py
+++ dolfin_adjoint_common/blocks/__init__.py
@@ -3,7 +3,6 @@
 from .assembly import AssembleBlock
 from .solving import GenericSolveBlock
 from .function import FunctionAssignBlock
-from .expression import ExpressionBlock
 from .dirichlet_bc import DirichletBCBlock
 from .constant import ConstantAssignBlock
 
