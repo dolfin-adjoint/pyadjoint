cc561e317c1ea8cbd25cbdc28ba7f483b86460e7
Stephan Kramer <s.kramer@imperial.ac.uk>
2020-04-07 16:44:33 +0100

    Also test splitting of non-mixed functions.
    This extends regression test for https://github.com/firedrakeproject/firedrake/issues/1632

--- tests/firedrake_adjoint/test_split.py
+++ tests/firedrake_adjoint/test_split.py
@@ -9,8 +9,18 @@ mesh = UnitSquareMesh(2, 2)
 cg2 = FiniteElement("CG", triangle, 2)
 cg1 = FiniteElement("CG", triangle, 1)
 ele = MixedElement([cg2, cg1])
-Z = FunctionSpace(mesh, ele)
+ZZ = FunctionSpace(mesh, ele)
 V2 = FunctionSpace(mesh, cg2)
+
+
+# the tests are run on functions from the MixedFunctionSpace ZZ
+# and on a normal (non-mixed) FunctionSpace. Calling split() on
+# a non-mixed function is trivial, but was previously broken
+@pytest.fixture(params=[ZZ, V2], ids=('mixed', 'non-mixed'))
+def Z(request):
+    return request.param
+
+
 rg = RandomGenerator()
 
 
@@ -32,17 +42,18 @@ def main(ic, fnsplit=True):
     return u
 
 
-def test_split():
+def test_split(Z):
     ic = Function(Z)
 
     u = main(ic, fnsplit=False)
     j = assemble(u**2*dx)
     rf = ReducedFunctional(j, Control(ic))
 
-    assert taylor_test(rf, ic.copy(deepcopy=True), h=project(Constant([1, 1]), ic.function_space())) > 1.9
+    h0 = Function(Z).assign(1.)
+    assert taylor_test(rf, ic.copy(deepcopy=True), h=h0) > 1.9
 
 
-def test_fn_split():
+def test_fn_split(Z):
     set_working_tape(Tape())
     ic = Function(Z)
 
@@ -55,7 +66,7 @@ def test_fn_split():
     assert taylor_test(rf, ic, h) > 1.9
 
 
-def test_fn_split_hessian():
+def test_fn_split_hessian(Z):
     set_working_tape(Tape())
     ic = Function(Z)
 
@@ -69,7 +80,7 @@ def test_fn_split_hessian():
     assert taylor_test(rf, ic, h, dJdm=dJdm, Hm=Hm) > 2.9
 
 
-def test_fn_split_no_annotate():
+def test_fn_split_no_annotate(Z):
     set_working_tape(Tape())
     ic = Function(Z)
 
@@ -95,8 +106,8 @@ def test_fn_split_no_annotate():
     assert min(r["R2"]["Rate"]) > 2.9
 
 
-def test_split_subvariables_update():
+def test_split_subvariables_update(Z):
     z = Function(Z)
-    u,v = z.split()
+    u = z.split()[0]
     u.project(Constant(1.))
     assert np.allclose(z.sub(0).vector().dat.data, u.vector().dat.data)
