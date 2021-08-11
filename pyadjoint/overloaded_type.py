a6da37d0b0c156c1c70e56f869728ef6552102ac
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-08-11 12:20:42 +0100

    Fix solve block

--- pyadjoint/overloaded_type.py
+++ pyadjoint/overloaded_type.py
@@ -346,7 +346,8 @@ class FloatingType(OverloadedType):
             return
 
         tape = get_working_tape()
-        block = self.block_class(*self._ad_args, ad_block_tag=self.ad_block_tag, **self._ad_kwargs)
+        block = self.block_class(*self._ad_args, **self._ad_kwargs)
+        block.ad_block_tag = self.ad_block_tag
         self.block = block
         tape.add_block(block)
         block.add_output(self.create_block_variable())
