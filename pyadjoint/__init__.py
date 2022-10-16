bc29b5069781d3afb54148ffc31c564ae2cd500d
Grant Bruer <grbr9001@gmail.com>
2022-10-16 15:47:20 -0400

    Import `compute_jacobian_action` in drivers.py

--- pyadjoint/__init__.py
+++ pyadjoint/__init__.py
@@ -14,7 +14,7 @@ from .tape import (Tape,
 from .adjfloat import AdjFloat
 from .reduced_functional import ReducedFunction, ReducedFunctional
 from .reduced_functional_numpy import ReducedFunctionNumPy, ReducedFunctionalNumPy
-from .drivers import compute_gradient, compute_hessian, solve_adjoint
+from .drivers import compute_gradient, compute_hessian, compute_jacobian_action, solve_adjoint
 from .verification import taylor_test, taylor_to_dict
 from .overloaded_type import OverloadedType, create_overloaded_object
 from .control import Control
