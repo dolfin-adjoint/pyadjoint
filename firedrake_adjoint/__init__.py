2641b6573691a3dd9bad52e7259193fcb3e9a90d
Connor Ward <c.ward20@imperial.ac.uk>
2023-08-18 12:43:47 +0100

    Fix firedrake_adjoint deprecation warning

--- firedrake_adjoint/__init__.py
+++ firedrake_adjoint/__init__.py
@@ -1,12 +1,12 @@
 # flake8: noqa
 
+import warnings
+
 from firedrake.adjoint import *
 
 continue_annotation()
 
-raise DeprecationWarning(
-    """The firedrake_adjoint module is deprecated.
+warnings.warn("""The firedrake_adjoint module is deprecated.
 
 Instead, use the firedrake.adjoint module and explicitly start taping
-by calling continue_annotation()."""
-)
+by calling continue_annotation().""", FutureWarning)
