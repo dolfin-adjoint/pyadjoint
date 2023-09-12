0f45c71ad41ce62995260d5ab1d789b01f758202
Iglesia Dolci <daia.dolci@gmail.com>
2023-09-12 10:13:25 +0100

    returning to the previous code structure

--- pyadjoint/tape.py
+++ pyadjoint/tape.py
@@ -126,12 +126,12 @@ def annotate_tape(kwargs=None):
     Returns: bool
 
     """
+    annotate = kwargs is None or kwargs.pop("annotate", True)
+
     # TODO: Consider if there is any scenario where one would want the keyword to have
     # precedence over the global flag.
     if not _annotation_enabled:
         return False
-
-    annotate = kwargs is None or kwargs.pop("annotate", True)
 
     return annotate
 
