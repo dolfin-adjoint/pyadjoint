9de3194b91ac8c66e78757505949697cd0525cb4
Iglesia Dolci <daia.dolci@gmail.com>
2024-02-23 14:11:25 +0000

    The checkpointing adapted for dynamic schedules and checkpointing test for a multistep problem.

--- tests/firedrake_adjoint/test_burgers_newton.py
+++ tests/firedrake_adjoint/test_burgers_newton.py
@@ -15,7 +15,7 @@ continue_annotation()
 n = 30
 mesh = UnitIntervalMesh(n)
 V = FunctionSpace(mesh, "CG", 2)
-end = 0.2
+end = 0.3
 timestep = Constant(1.0/n)
 steps = int(end/float(timestep)) + 1
 
@@ -41,18 +41,20 @@ def J(ic, solve_type, checkpointing):
 
     tape = get_working_tape()
     t += float(timestep)
-    for t in tape.timestepper(np.arange(t, end + t, float(timestep))):
+    for t in tape.timestepper(range(steps)):
+        print(float(timestep)*t)
         if solve_type == "NLVS":
             solver.solve()
         else:
             solve(F == 0, u, bc)
         u_.assign(u)
 
-    return assemble(u_*u_*dx + ic*ic*dx), u_
+    return assemble(u_*u_*dx + ic*ic*dx)
 
 
 @pytest.mark.parametrize("solve_type, checkpointing",
-                         [("solve", "Revolve"),
+                         [
+                          ("solve", "Revolve"),
                           ("NLVS", "Revolve"),
                           ("solve", "SingleMemory"),
                           ("NLVS", "SingleMemory"),
@@ -80,58 +82,55 @@ def test_burgers_newton(solve_type, checkpointing):
         tape.enable_checkpointing(schedule)
     x, = SpatialCoordinate(mesh)
     ic = project(sin(2.* pi * x), V)
-    val, _ = J(ic, solve_type, checkpointing)
+    val = J(ic, solve_type, checkpointing)
     if checkpointing:
         assert len(tape.timesteps) == steps
-    # if checkpointing != "NoneAdjoint":
-    #     Jhat = ReducedFunctional(val, Control(ic))
-    # dJ = Jhat.derivative()
+    Jhat = ReducedFunctional(val, Control(ic))
+    if checkpointing != "NoneAdjoint":
+        dJ = Jhat.derivative()
 
-    # # Recomputing the functional with a modified control variable
-    # # before the recompute test.
-    # Jhat(project(sin(pi*x), V))
+    # Recomputing the functional with a modified control variable
+    # before the recompute test.
+    Jhat(project(sin(pi*x), V))
 
-    # # Recompute test
-    # assert(np.allclose(Jhat(ic), val))
-    # if checkpointing != "NoneAdjoint":
-    #     dJbar = Jhat.derivative()
-    #     # Test recompute adjoint-based gradient
-    #     assert np.allclose(dJ.dat.data_ro[:], dJbar.dat.data_ro[:])
-
-    #     # Taylor test
-    #     h = Function(V)
-    #     h.assign(1, annotate=False)
-    #     assert taylor_test(Jhat, ic, h) > 1.9
+    # Recompute test
+    assert(np.allclose(Jhat(ic), val))
+    if checkpointing != "NoneAdjoint":
+        dJbar = Jhat.derivative()
+        # Test recompute adjoint-based gradient
+        assert np.allclose(dJ.dat.data_ro[:], dJbar.dat.data_ro[:])
+        # Taylor test
+        assert taylor_test(Jhat, ic, Function(V).assign(1, annotate=False)) > 1.9
 
 
-# @pytest.mark.parametrize("solve_type, checkpointing",
-#                          [("solve", "Revolve"),
-#                           ("NLVS", "Revolve")
-#                           ])
-# def test_checkpointing_validity(solve_type, checkpointing):
-#     """Compare forward and backward results with and without checkpointing.
-#     """
-#     # Without checkpointing
-#     tape = get_working_tape()
-#     tape.progress_bar = ProgressBar
-#     x, = SpatialCoordinate(mesh)
-#     ic = project(sin(2.*pi*x), V)
+@pytest.mark.parametrize("solve_type, checkpointing",
+                         [("solve", "Revolve"),
+                          ("NLVS", "Revolve"),
+                          ("solve", "Mixed"),
+                          ("NLVS", "Mixed"),
+                          ])
+def test_checkpointing_validity(solve_type, checkpointing):
+    """Compare forward and backward results with and without checkpointing.
+    """
+    # Without checkpointing
+    tape = get_working_tape()
+    tape.progress_bar = ProgressBar
+    x, = SpatialCoordinate(mesh)
+    ic = project(sin(2.*pi*x), V)
 
-#     val0, u0 = J(ic, solve_type, False)
-#     Jhat = ReducedFunctional(val0, Control(ic))
-#     dJ0 = Jhat.derivative()
-#     tape.clear_tape()
+    val0 = J(ic, solve_type, False)
+    Jhat = ReducedFunctional(val0, Control(ic))
+    dJ0 = Jhat.derivative()
+    tape.clear_tape()
 
-#     # With checkpointing
-#     tape.progress_bar = ProgressBar
-#     if checkpointing == "Revolve":
-#         tape.enable_checkpointing(Revolve(steps, steps//3))
-#     x, = SpatialCoordinate(mesh)
-#     ic = project(sin(2.*pi*x), V)
-#     val1, u1 = J(ic, solve_type, True)
-#     Jhat = ReducedFunctional(val1, Control(ic))
-#     dJ1 = Jhat.derivative()
-#     assert len(tape.timesteps) == steps
-#     assert np.allclose(val0, val1)
-#     assert np.allclose(u0.dat.data_ro[:], u1.dat.data_ro[:])
-#     assert np.allclose(dJ0.dat.data_ro[:], dJ1.dat.data_ro[:])
+    # With checkpointing
+    tape.progress_bar = ProgressBar
+    if checkpointing == "Revolve":
+        tape.enable_checkpointing(Revolve(steps, steps//3))
+    if checkpointing == "Mixed":
+        tape.enable_checkpointing(MixedCheckpointSchedule(steps, steps//3, storage=StorageType.RAM))
+    val1 = J(ic, solve_type, True)
+    Jhat = ReducedFunctional(val1, Control(ic))
+    assert len(tape.timesteps) == steps
+    assert np.allclose(val0, val1)
+    assert np.allclose(dJ0.dat.data_ro[:], Jhat.derivative().dat.data_ro[:])
