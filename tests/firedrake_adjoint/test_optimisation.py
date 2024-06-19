a6aec148005fdb3a2ea538525a83ab6a5d13c995
Ig-dolci <daia.dolci@gmail.com>
2024-06-19 12:42:55 +0100

    wip

--- tests/firedrake_adjoint/test_optimisation.py
+++ tests/firedrake_adjoint/test_optimisation.py
@@ -2,27 +2,28 @@ import pytest
 pytest.importorskip("firedrake")
 
 from numpy.testing import assert_allclose
+import numpy as np
 from firedrake import *
 from firedrake.adjoint import *
 
 
-def test_optimisation_constant_control():
-    """This tests a list of controls in a minimisation (through scipy L-BFGS-B)"""
-    mesh = UnitSquareMesh(1, 1)
-    R = FunctionSpace(mesh, "R", 0)
+# def test_optimisation_constant_control():
+#     """This tests a list of controls in a minimisation (through scipy L-BFGS-B)"""
+#     mesh = UnitSquareMesh(1, 1)
+#     R = FunctionSpace(mesh, "R", 0)
 
-    n = 3
-    x = [Function(R) for i in range(n)]
-    c = [Control(xi) for xi in x]
+#     n = 3
+#     x = [Function(R) for i in range(n)]
+#     c = [Control(xi) for xi in x]
 
-    # Rosenbrock function https://en.wikipedia.org/wiki/Rosenbrock_function
-    # with minimum at x = (1, 1, 1, ...)
-    f = sum(100*(x[i+1] - x[i]**2)**2 + (1 - x[i])**2 for i in range(n-1))
+#     # Rosenbrock function https://en.wikipedia.org/wiki/Rosenbrock_function
+#     # with minimum at x = (1, 1, 1, ...)
+#     f = sum(100*(x[i+1] - x[i]**2)**2 + (1 - x[i])**2 for i in range(n-1))
 
-    J = assemble(f * dx(domain=mesh))
-    rf = ReducedFunctional(J, c)
-    result = minimize(rf)
-    assert_allclose([float(xi) for xi in result], 1., rtol=1e-4)
+#     J = assemble(f * dx(domain=mesh))
+#     rf = ReducedFunctional(J, c)
+#     result = minimize(rf)
+#     assert_allclose([float(xi) for xi in result], 1., rtol=1e-4)
 
 
 def _simple_helmholz_model(V, source):
@@ -56,3 +57,10 @@ def test_simple_inversion():
 
     x = minimize(rf)
     assert_allclose(x.dat.data, source_ref.dat.data, rtol=1e-2)
+    rf(source)
+    x = minimize(rf, derivative_options={"riesz_representation": "l2"})
+    assert_allclose(x.dat.data, source_ref.dat.data, rtol=1e-2)
+    rf(source)
+    x = minimize(rf, derivative_options={"riesz_representation": "H1"})
+    # Assert that the optimisation does not converge for H1 representation
+    assert not np.allclose(x.dat.data, source_ref.dat.data, rtol=1e-2)
