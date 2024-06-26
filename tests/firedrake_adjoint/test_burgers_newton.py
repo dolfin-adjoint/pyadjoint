7eeb49aef20203f0e910b7081c9641b6a4bcd87a
Daiane Iglesia Dolci <63597005+Ig-dolci@users.noreply.github.com>
2024-06-26 13:19:24 +0100

    Dynamic schedules (#137)
    * The checkpointing adapted for dynamic schedules and the addition of a test for linear multistep employment.

--- tests/firedrake_adjoint/test_burgers_newton.py
+++ tests/firedrake_adjoint/test_burgers_newton.py
@@ -7,7 +7,8 @@ pytest.importorskip("firedrake")
 
 from firedrake import *
 from firedrake.adjoint import *
-from checkpoint_schedules import Revolve, MultistageCheckpointSchedule
+from checkpoint_schedules import Revolve, SingleMemoryStorageSchedule, MixedCheckpointSchedule,\
+    NoneCheckpointSchedule, StorageType
 import numpy as np
 set_log_level(CRITICAL)
 continue_annotation()
@@ -33,28 +34,31 @@ def J(ic, solve_type, checkpointing):
          + u*u.dx(0)*v + nu*u.dx(0)*v.dx(0))*dx
     bc = DirichletBC(V, 0.0, "on_boundary")
 
-    t = 0.0
     if solve_type == "NLVS":
         problem = NonlinearVariationalProblem(F, u, bcs=bc)
         solver = NonlinearVariationalSolver(problem)
 
     tape = get_working_tape()
-    t += float(timestep)
-    for t in tape.timestepper(np.arange(t, end + t, float(timestep))):
+    for _ in tape.timestepper(range(steps)):
         if solve_type == "NLVS":
             solver.solve()
         else:
             solve(F == 0, u, bc)
         u_.assign(u)
 
-    return assemble(u_*u_*dx + ic*ic*dx), u_
+    return assemble(u_*u_*dx + ic*ic*dx)
 
 
 @pytest.mark.parametrize("solve_type, checkpointing",
-                         [("solve", "Revolve"),
+                         [
+                          ("solve", "Revolve"),
                           ("NLVS", "Revolve"),
-                          ("solve", "Multistage"),
-                          ("NLVS", "Multistage"),
+                          ("solve", "SingleMemory"),
+                          ("NLVS", "SingleMemory"),
+                          ("solve", "NoneAdjoint"),
+                          ("NLVS", "NoneAdjoint"),
+                          ("solve", "Mixed"),
+                          ("NLVS", "Mixed"),
                           ("solve", None),
                           ("NLVS", None),
                           ])
@@ -63,18 +67,24 @@ def test_burgers_newton(solve_type, checkpointing):
     """
     tape = get_working_tape()
     tape.progress_bar = ProgressBar
-    if checkpointing == "Revolve":
-        tape.enable_checkpointing(Revolve(steps, steps//3))
-    if checkpointing == "Multistage":
-        tape.enable_checkpointing(MultistageCheckpointSchedule(steps, steps//3, 0))
+    if checkpointing:
+        if checkpointing == "Revolve":
+            schedule = Revolve(steps, steps//3)
+        if checkpointing == "SingleMemory":
+            schedule = SingleMemoryStorageSchedule()
+        if checkpointing == "Mixed":
+            schedule = MixedCheckpointSchedule(steps, steps//3, storage=StorageType.RAM)
+        if checkpointing == "NoneAdjoint":
+            schedule = NoneCheckpointSchedule()
+        tape.enable_checkpointing(schedule)
     x, = SpatialCoordinate(mesh)
-    ic = project(sin(2.*pi*x), V)
-    val, _ = J(ic, solve_type, checkpointing)
+    ic = project(sin(2. * pi * x), V)
+    val = J(ic, solve_type, checkpointing)
     if checkpointing:
         assert len(tape.timesteps) == steps
-
     Jhat = ReducedFunctional(val, Control(ic))
-    dJ = Jhat.derivative()
+    if checkpointing != "NoneAdjoint":
+        dJ = Jhat.derivative()
 
     # Recomputing the functional with a modified control variable
     # before the recompute test.
@@ -82,22 +92,19 @@ def test_burgers_newton(solve_type, checkpointing):
 
     # Recompute test
     assert(np.allclose(Jhat(ic), val))
-
-    dJbar = Jhat.derivative()
-    # Test recompute adjoint-based gradient
-    assert np.allclose(dJ.dat.data_ro[:], dJbar.dat.data_ro[:])
-
-    # Taylor test
-    h = Function(V)
-    h.assign(1, annotate=False)
-    assert taylor_test(Jhat, ic, h) > 1.9
+    if checkpointing != "NoneAdjoint":
+        dJbar = Jhat.derivative()
+        # Test recompute adjoint-based gradient
+        assert np.allclose(dJ.dat.data_ro[:], dJbar.dat.data_ro[:])
+        # Taylor test
+        assert taylor_test(Jhat, ic, Function(V).assign(1, annotate=False)) > 1.9
 
 
 @pytest.mark.parametrize("solve_type, checkpointing",
                          [("solve", "Revolve"),
                           ("NLVS", "Revolve"),
-                          ("solve", "Multistage"),
-                          ("NLVS", "Multistage")
+                          ("solve", "Mixed"),
+                          ("NLVS", "Mixed"),
                           ])
 def test_checkpointing_validity(solve_type, checkpointing):
     """Compare forward and backward results with and without checkpointing.
@@ -108,7 +115,7 @@ def test_checkpointing_validity(solve_type, checkpointing):
     x, = SpatialCoordinate(mesh)
     ic = project(sin(2.*pi*x), V)
 
-    val0, u0 = J(ic, solve_type, False)
+    val0 = J(ic, solve_type, False)
     Jhat = ReducedFunctional(val0, Control(ic))
     dJ0 = Jhat.derivative()
     tape.clear_tape()
@@ -117,14 +124,10 @@ def test_checkpointing_validity(solve_type, checkpointing):
     tape.progress_bar = ProgressBar
     if checkpointing == "Revolve":
         tape.enable_checkpointing(Revolve(steps, steps//3))
-    if checkpointing == "Multistage":
-        tape.enable_checkpointing(MultistageCheckpointSchedule(steps, steps//3, 0))
-    x, = SpatialCoordinate(mesh)
-    ic = project(sin(2.*pi*x), V)
-    val1, u1 = J(ic, solve_type, True)
+    if checkpointing == "Mixed":
+        tape.enable_checkpointing(MixedCheckpointSchedule(steps, steps//3, storage=StorageType.RAM))
+    val1 = J(ic, solve_type, True)
     Jhat = ReducedFunctional(val1, Control(ic))
-    dJ1 = Jhat.derivative()
     assert len(tape.timesteps) == steps
     assert np.allclose(val0, val1)
-    assert np.allclose(u0.dat.data_ro[:], u1.dat.data_ro[:])
-    assert np.allclose(dJ0.dat.data_ro[:], dJ1.dat.data_ro[:])
+    assert np.allclose(dJ0.dat.data_ro[:], Jhat.derivative().dat.data_ro[:])
