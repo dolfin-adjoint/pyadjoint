e9a709050c3771f1ed3f3822c5d6a7b15999e491
James R. Maddison <j.r.maddison@ed.ac.uk>
2024-08-05 11:59:36 +0100

    Update pyadjoint/optimization/tao_solver.py
    Co-authored-by: Daiane Iglesia Dolci <63597005+Ig-dolci@users.noreply.github.com>

--- pyadjoint/optimization/tao_solver.py
+++ pyadjoint/optimization/tao_solver.py
@@ -132,9 +132,7 @@ class PETScVecInterface:
             if isinstance(y, Complex):
                 x_sub.set(y)
             elif isinstance(y, OverloadedType):
-                y_vec = y._ad_to_petsc()
-                y_vec.copy(result=x_sub)
-                y_vec.destroy()
+                x_sub = y._ad_to_petsc(vec=x_sub)
             else:
                 raise TypeError(f"Unexpected type: {type(y)}")
             x_sub.restoreSubVector(iset, x_sub)
