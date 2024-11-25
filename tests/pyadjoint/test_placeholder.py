db89a053f90e951906a748f5acfa7fd4fee256f2
Iglesia Dolci <daia.dolci@gmail.com>
2024-11-25 13:40:10 +0000

    Tests fixing

--- tests/pyadjoint/test_placeholder.py
+++ tests/pyadjoint/test_placeholder.py
@@ -12,21 +12,21 @@ def test_simple():
     e = c*d
 
     Jhat = ReducedFunctional(e, Control(a))
-    assert Jhat(2.0) == e
+    assert Jhat(AdjFloat(2.0)) == e
     p.set_value(e)
-    res = Jhat(2.0)
+    res = Jhat(AdjFloat(2.0))
     assert res != e
     assert res == e*6
-    assert Jhat(2.0) == e * 6 ** 2
-    assert Jhat(2.0) == e * 6 ** 3
-    assert Jhat(2.0) == e * 6 ** 4
+    assert Jhat(AdjFloat(2.0)) == e * 6 ** 2
+    assert Jhat(AdjFloat(2.0)) == e * 6 ** 3
+    assert Jhat(AdjFloat(2.0)) == e * 6 ** 4
     # The functional changed after last evaluation so the derivative is with the new placeholder value:
     assert Jhat.derivative() == 0.5 * e * 6 ** 5
 
     Jhat.optimize_tape()
-    assert Jhat(2.0) == e * 6 ** 5
+    assert Jhat(AdjFloat(2.0)) == e * 6 ** 5
     p.set_value(a)
-    assert Jhat(2.0) == 2.0 * 6
+    assert Jhat(AdjFloat(2.0)) == 2.0 * 6
     assert Jhat.derivative() == 0.5 * 2.0 * 6
-    assert Jhat(5.0) == 5.0 * 15
+    assert Jhat(AdjFloat(5.0)) == 5.0 * 15
     assert Jhat.derivative() == 15
