7ac322021a945a3e69e241ce0649715e4c83dbdf
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-02-20 15:11:44 +0000

    Removed redundant check for parameters being instance of `Control`. The user is not expected to pass `parameters` wrapped in `Control`

--- pyadjoint/reduced_functional.py
+++ pyadjoint/reduced_functional.py
@@ -442,7 +442,7 @@ class ParametrisedReducedFunctional(AbstractReducedFunctional):
         self._controls = Enlist(controls)
         self._parameters = Enlist(parameters)
         self.n_opt = len(self._controls)
-        self._all_controls= self._controls + Enlist([Control(p) if not isinstance(p, Control) else p for p in self._parameters])
+        self._all_controls= self._controls + Enlist([Control(p) for p in self._parameters])
 
         self._reduced_functional = ReducedFunctional(functional=functional, 
                                                      controls=self._all_controls, 
